3e59a740a1b933b02e732a75b0ddbf1267d79726
Wietse Neven <hello@wietseneven.com>
2018-04-10 22:52:18 +0200

    Changed order of candidates

--- src/Components/templates/Trends.vue
+++ src/Components/templates/Trends.vue
@@ -150,10 +150,10 @@
         if (!this.single) {
           this.filter = [
             {name: 'Iván Duque', id: this.$store.getters.getCandidateByName('Iván Duque').trendsId},
-            {name: 'Germán Vargas Lleras', id: this.$store.getters.getCandidateByName('Germán Vargas Lleras').trendsId},
-            {name: 'Sergio Fajardo', id: this.$store.getters.getCandidateByName('Sergio Fajardo').trendsId},
             {name: 'Humberto de la Calle', id: this.$store.getters.getCandidateByName('Humberto de la Calle').trendsId},
-            {name: 'Gustavo Petro', id: this.$store.getters.getCandidateByName('Gustavo Petro').trendsId}
+            {name: 'Gustavo Petro', id: this.$store.getters.getCandidateByName('Gustavo Petro').trendsId},
+            {name: 'Sergio Fajardo', id: this.$store.getters.getCandidateByName('Sergio Fajardo').trendsId},
+            {name: 'Germán Vargas Lleras', id: this.$store.getters.getCandidateByName('Germán Vargas Lleras').trendsId}
           ]
         } else {
           this.filter = [{name: 'Iván Duque', id: this.$store.getters.getCandidateByName('Iván Duque').trendsId}]
